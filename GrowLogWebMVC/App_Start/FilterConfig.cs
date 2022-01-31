58d92785f02b540f6b01550b8261e73a75e624c5
Maggie <88403063+maggiesnader@users.noreply.github.com>
2022-01-31 11:17:19 -0500

    Add project files.

---
+++ GrowLogWebMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GrowLogWebMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
