e5cdee92f240f8b4a587de53804eec7cc2ac2218
Maggie <88403063+maggiesnader@users.noreply.github.com>
2022-01-31 15:46:02 -0500

    Basic DataLayer, Connection String setup

---
+++ GrowLog.Data/LogEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowLog.Data
+{
+    public class LogEntry
+    {
+        [Key]
+        public int LogEntryID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [ForeignKey("Plant")]
+        public int PlantID { get; set; }
+        public virtual Plant Plant { get; set; }
+    }
+}
---
+++ GrowLog.Data/Plant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowLog.Data
+{
+    public class Plant
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+        
+        public TimeSpan HarvestSeason { get; set; }
+        
+        public TimeSpan PlantingSeason { get; set; }
+        
+        [Required]
+        public PlantCategory TypeOfPlantCategory { get; set; }
+
+        [ForeignKey("Location")]
+        public int LocationID { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
