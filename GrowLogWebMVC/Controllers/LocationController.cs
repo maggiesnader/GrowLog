c1fddf311f1058a712f9b22cad75465f4d3ec08f
Maggie <88403063+maggiesnader@users.noreply.github.com>
2022-01-31 16:03:33 -0500

    Location Index View / Authorize

---
+++ GrowLogWebMVC/Controllers/LocationController.cs
@@ -0,0 +1,20 @@
+﻿using GrowLog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrowLogWebMVC.Controllers
+{
+    [Authorize]
+    public class LocationController : Controller
+    {
+        // GET: Location
+        public ActionResult Index()
+        {
+            var model = new LocationListItem[0];
+            return View(model);
+        }
+    }
+}
